8cae99c4c7afd4b016c329acf656414f478b3696
AmitPawar52 <pawaramit10229@gmail.com>
2021-06-12 14:28:21 +0530

    Demo project for spring boot with JSP

---
+++ src/main/java/com/store/medical/TestProjectApplication.java
@@ -0,0 +1,18 @@
+package com.store.medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectApplication {
+
+//	@Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(TestProjectApplication.class);
+//    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/medical/TestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.medical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
